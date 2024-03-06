209c63665faee3f3bc235dc4bd93d95b5441b39f
rdwangyu <rdwangyu@foxmail.com>
2024-03-07 06:40:18 +0800

    use same db as server

--- moreway_server/settings-development.py
+++ moreway_server/settings-development.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'moreway_server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'moreway',
         'USER': 'postgres',
         'PASSWORD': '123456',
         'HOST': '127.0.0.1',
